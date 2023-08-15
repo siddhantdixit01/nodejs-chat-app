2fe9fa7e5d29dd1233b290039f6ae92c18a7db72
Siddhant Dixit <114592704+siddhantdixit01@users.noreply.github.com>
2023-08-15 17:26:38 +0530

    commit

--- index.js
+++ index.js
@@ -40,7 +40,7 @@ io.on('connection', (socket) => {
     console.log('A user disconnected');
   });
 });
-const PORT = "process.env.PORT || 3000";
+const PORT = process.env.PORT || 3000;
 server.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
